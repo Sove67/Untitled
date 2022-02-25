6678212f83ad05dcffeb3519071f6a74a9d1e622
Kaleb Afseth <afseth598@gmail.com>
2022-02-24 23:33:52 -0800

    Sprites & Pixel Perfection
    Created some player sprites, setup a pixel perfect camera

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D body;
+    private SpriteRenderer sprite;
+    private Vector2 input;
+
+    private void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+            ).normalized;
+        // Flip Sprite to match X axis
+        if (input.x != 0)
+        { sprite.flipX = input.x > 0; }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 target = body.position + input * moveSpeed * Time.fixedDeltaTime;
+        body.MovePosition(target);
+    }
+}
